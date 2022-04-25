c3d01a8707fb2e63b507b22f7fa8fa284c6510e1
АlekseyANel <71459001+AlekseyANel@users.noreply.github.com>
2022-04-25 20:06:48 +0300

    Delete Hermle.java

--- src/com/alevel/Hermle.java
+++
@@ -1,9 +0,0 @@
-package com.alevel;
-
-public class Hermle implements Clocks{
-
-    @Override
-    public void writeClocks() {
-        System.out.println("В 1922-м году немец Франц Хермле основал компанию Hermle.");
-    }
-}
